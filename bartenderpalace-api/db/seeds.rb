202f666c9c23ddab90372237136b87e91fbd3ca4
h-miiaah <haim.miiaah5@gmail.com>
2020-06-13 19:52:35 -0400

    added seed data

--- bartenderpalace-api/db/seeds.rb
+++ bartenderpalace-api/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Drink.create([
+    {name: "Old Fashioned", description: "The Old Fashioned is as classic as they come, and perfect for anyone who likes their whiskey drinks on the sweeter side. Rye will make it spicier. Place sugar in an Old Fashioned glass. Wet it down with Angostura bitters and a splash of club soda, then muddle. Add a large ice cube and whiskey. Stir, then garnish with an orange twist."},
+    {name: "Whiskey Sour", description: "For those nights when you want to cleanse your palate, opt for a puckery Whiskey Sour. Specify bourbon for this one. Shake ingredients well with cracked ice, strain into a chilled cocktail glass, and garnish with a lemon wedge."},
+    {name: "Daiquiri", description: "Speaking of summertime cocktails, the Daiquiri is a citrusy marriage of rum and lime (and the preferred drink of Ernest Hemingway). Strained is preferred. Squeeze lime into a shaker, stir in sugar, then add rum. Shake well with cracked ice and strain into a chilled cocktail glass."},
+    {name: "Manhattan", description: "While quality whiskey is important, the key to a great Manhattan is the sweet vermouth. Stir ingredients well with cracked ice, then strain into a chilled cocktail glass. Garnish with a twist or maraschino cherry."},
+    {name: "Margarita", description: "The Margarita is the workhorse of summer. Its best made with a splash of Cointreau or Grand Marnier, and if you like it a little sweeter, ask your bartender to add agave syrup."},
+    {name: "Cosmopolitan", description: "The cosmo became almost ubiquitous in the '90s thanks to the TV show Sex and the City, but this spin on the martini remains just as tasty today as when Carrie Bradshaw made it famous. Build all ingredients in a shaker tine with ice and shake. Strain into a martini glass and garnish with lime wheel or zest."},
+])
