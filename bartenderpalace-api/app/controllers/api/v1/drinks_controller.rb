de6661591383aa92565f8c6dacf8fcbdb582e73f
h-miiaah <haim.miiaah5@gmail.com>
2020-06-13 17:49:49 -0400

    added show and create actions with strong params

--- bartenderpalace-api/app/controllers/api/v1/drinks_controller.rb
+++ bartenderpalace-api/app/controllers/api/v1/drinks_controller.rb
@@ -3,4 +3,20 @@ class Api::V1::DrinksController < ApplicationController
         @drinks = Drink.all
         render json: @drinks, status: 200
     end
+
+    def show
+        @drink = Dring.find(params[:id])
+        render json: @drink, status: 200
+    end
+
+    def create
+        @drink = Drink.create(drink_params)
+        render json: @drink, status: 200
+    end
+
+    private
+
+    def drink_params
+        params.require(:drink).permit(:name, :decription)
+    end
 end
