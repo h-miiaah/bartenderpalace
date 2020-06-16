06b669d0a5a8a53696ee64ca21dc0b32bdfd5506
h-miiaah <haim.miiaah5@gmail.com>
2020-06-15 22:03:43 -0400

    added pry gem to api direcroty gemfile

--- bartenderpalace-api/app/controllers/api/v1/drinks_controller.rb
+++ bartenderpalace-api/app/controllers/api/v1/drinks_controller.rb
@@ -10,6 +10,7 @@ class Api::V1::DrinksController < ApplicationController
     end
 
     def create
+        binding.pry
         @drink = Drink.create(drink_params)
         render json: @drink, status: 200
     end
