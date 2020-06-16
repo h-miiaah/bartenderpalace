2353c3602481a00d5dfb13bb3097b2b79805ea88
h-miiaah <haim.miiaah5@gmail.com>
2020-06-15 21:21:19 -0400

    added ability to add the drinks in the drinks array to the drinks container in the dom inside the render().

--- js-frontend/src/components/drinks.js
+++ js-frontend/src/components/drinks.js
@@ -17,9 +17,14 @@ class Drinks {
       });
   }
 
-  //   adds drinks to the drinks-container div on the DOM.
+  //   adds drinks to the drinks-container div on the DOM. Once we hit this render function we have access to an array of drinks (you can console log this.drinks to see).
+  //   we set the innerHTML of the drinks container by mapping through the array fo drinks from this.drinks and for each drink we set the html property we want to have in the dom, then join the string we get back with the join method so it displays properly without a "," between each drink.
   render() {
     const drinksContainer = document.getElementById("drinks-container");
-    drinksContainer.innerHTML = "drinks go here";
+    drinksContainer.innerHTML = this.drinks
+      .map(
+        (drink) => `<h4><li>${drink.name}</li></h4> <p>${drink.description}</p>`
+      )
+      .join("");
   }
 }
